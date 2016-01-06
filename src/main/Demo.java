14940feb4454805f01fedf0186ae0dee07e48d02
Siva Kumar <kumar.hsiva@tcs.com>
2016-01-06 23:26:22 +0530

    First Version
    Change-Id: I9f35ec836a7f1e9ad23725dd1d1c6afc8d169234

---
+++ src/main/Demo.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("sum of 1 and 4 is: " + add(1,4));
+	}
+	
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+}
