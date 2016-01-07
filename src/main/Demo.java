485e55659bfae5cdf380fe9b3b2496e151fdef73
Siva Kumar <kumar.hsiva@tcs.com>
2016-01-07 10:13:46 +0530

    test

--- src/main/Demo.java
+++ src/main/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("sum of 1 and 4 is: " + add(1,4));
+		System.out.println("sum of 1 and 5 is: " + add(1,5));
 	}
 	
 	public static int add(int a, int b) {
